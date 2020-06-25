5b0583a5c72002fbb604a97dcbaa1d5658767262
Dmytro Shevchenko <dimashevchenko10021999@gmail.com>
2020-06-25 13:58:47 +0300

    Implement todo list
    :Release Notes:
    
    :Detailed Notes:
    
    :Testing Performed:
    No tests
    
    :QA Notes:
    
    :Issues Addressed:
    
    Signed-off-by: Shevchenko Dmytro <dimashevchenko10021999@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,29 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Todo application</h1>
+    <hr />
+    <TodoList v-bind:todos="todos" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoList from "@/components/TodoList";
 
 export default {
-  name: 'App',
+  name: "App",
+  data() {
+    return {
+      todos: [
+        { id: 1, title: "Купить хлеб", complated: false },
+        { id: 2, title: "Купить молоко", complated: false },
+        { id: 3, title: "Купить воду", complated: false }
+      ]
+    };
+  },
   components: {
-    HelloWorld
+    TodoList
   }
-}
+};
 </script>
 
 <style>
